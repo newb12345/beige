2bc526babac30387eea5e44d7fb461d75aa6ab2a
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-03-29 15:16:53 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('hiya!');
+    if (message.content === '>ladder') {
+    	message.reply('FAQ - https://www.taptap.com/topic/2666165 \n Q: Are there any requirements for ladder? \n A: There are no entrance fee or requirements.  You can play as many times as you want each day.Q: How often are the songs updated?');
   	}
 });
 
