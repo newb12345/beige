3ee93ac81f85b222edc4fc4045684eb61fdb675b
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-03-29 16:02:30 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ client.on('message', message => {
         \n -No, the songs are pre-determined and one random one will be picked from the song list. \
         \n\n **Source** - https://www.taptap.com/topic/2666165 \n'); 	
         }
-      if (message.content === 'hello, @Beige#7628') {
+      if (message.content === 'hello') {
           message.reply('hi~');
       }
 });
