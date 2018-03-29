6be586295180b8843b3c669f4dac0a8677a31e1d
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-03-29 15:51:48 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,11 +7,11 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === '>ladder') {
-    	message.channel.send('\n **Are there any requirements for ladder?** \
+    	message.channel.send('\n __*Are there any requirements for ladder?*__ \
         \n -There are no entrance fee or requirements.  You can play as many times as you want each day. \
-        \n **How often are the songs updated?** \
-        \n -Once every 48 hours.  It is worth mentioning that the songs will contain Platinum-only songs,so non-Platinum members get to experience them too. \
-        \n **Can we pick the song(s) we want to play?** \
+        \n __*How often are the songs updated?*__ \
+        \n -Once every 48 hours.  It is worth mentioning that the songs will contain Platinum-only songs, so non-Platinum members get to experience them too. \
+        \n __*Can we pick the song(s) we want to play?*__ \
         \n -No, the songs are pre-determined and one random one will be picked from the song list. \
         \n **Source** - https://www.taptap.com/topic/2666165 \n'); 	
         }
