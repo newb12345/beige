bd9a8e932610324f641310a2a0fa1a5b7da268bc
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-03-30 17:53:00 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -52,7 +52,15 @@ client.on('message', async message => {
          var tips = fs.readFileSync("./source/tips.txt", {"encoding": "utf-8"});
 	     message.channel.send(tips);
         }
-	
+	if command === "addtip") {
+		 let tip = args.slice(1).join(' ');
+		  if(!tip)
+   			   return message.reply("Ya! You forgot to include the tip!");
+	 	 let cat = args[0];
+		   if(!cat)
+   			   return message.reply("Eyyy...Choose what category is the tip first.");
+		message.channel.send("Thanks! Your tip will be added after the moderators read it. --'");
+	}
     
 });
 
