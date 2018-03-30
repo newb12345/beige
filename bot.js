03aa271edb8fbd222bb9538e758debd1e8c02682
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-03-30 16:09:13 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -53,7 +53,7 @@ client.on('message', async message => {
 	     message.channel.send(read);
 
  	sql.get(`SELECT * FROM addtips WHERE category ="${cat}"`).then(row => {
-            message.channel.send(`Tips in ladder: ${row.suggestion}`)
+            message.channel.send(`Tips in ladder: ${row.suggestion[1]}`)
        		 });	     
 	      
         } 
