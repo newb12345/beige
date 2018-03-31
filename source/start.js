9acb4fe36cf41466aeca609327e72ec97ec91dd8
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-01 00:24:51 +0800

    Update start.js

--- source/start.js
+++ source/start.js
@@ -5,15 +5,15 @@ const Discord = require("discord.js");
 var embed = new Discord.RichEmbed()
   .setTitle("Ladder")
  // .setAuthor(client.user.username, "https://i.imgur.com/TgNMUlS.jpg")
-  .setColor(3447003);
- /* .setDescription("Ladder FAQ")
+  .setColor(3447003)
+  .setDescription("Ladder FAQ")
   .setFooter("Brought to you by: BlinKcle, weiwei, Steve", "http://i.imgur.com/w1vhFSR.png")
   .setThumbnail("http://i.imgur.com/p2qNFag.png")
   /*
    * Takes a Date object, defaults to current date.
-   *//*
-  .setTimestamp()
-  .addField("Q: How often are the songs updated?",
+   */
+  .setTimestamp();
+/*  .addField("Q: How often are the songs updated?",
     "A: Once every 48 hours.  It is worth mentioning that the songs will contain Platinum-only songs, so non-Platinum members get to experience them too.", true)
   .addField("Q: Can we pick the song(s) we want to play?", "A: No, the songs are pre-determined and one random song will be chosen for you from the song list.", true)
   /*
