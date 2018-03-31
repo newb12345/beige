87ef8c9527e15a6d8dd6064e108ea088812ce530
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-01 00:13:34 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,7 +32,7 @@ const categ = ["ladder", "club", "roadshow","ranking","krunk-dorm", "others"];
   //faq's on ladder  
     switch(command) {
 		case 'ladder': 
-		      message.channel.send(start.embed);
+		//      message.channel.send(start.embed);
 					     break;
          		//var com = fs.readFileSync("./source/ladder.txt", {"encoding": "utf-8"});
 		    
