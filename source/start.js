a0acd490e5d2733531734f580928da915e3c5d2c
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-03-31 23:04:16 +0800

    Update start.js

--- source/start.js
+++ source/start.js
@@ -1,7 +1,5 @@
 // start.js
 
-module.exports.embed =  {
-                        color: 3447003,
-                        description: "A very simple Embed!"
-                          };
+module.exports.embed =  new Discord.RichEmbed()
+                        .setTitle("This is your title, it can hold 256 characters");
 module.exports.data = 1234;
