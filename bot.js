64602a86371f815780b797245220186b52540994
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-03-31 11:49:20 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,34 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 //sql
+const embed = new Discord.RichEmbed()
+  .setTitle("Ladder")
+  .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
+  /*
+   * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+   */
+  .setColor(0x00AE86)
+  .setDescription("This is the main body of text, it can hold 2048 characters.")
+  .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
+  .setImage("http://i.imgur.com/yVpymuV.png")
+  .setThumbnail("http://i.imgur.com/p2qNFag.png")
+  /*
+   * Takes a Date object, defaults to current date.
+   */
+  .setTimestamp()
+  .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
+  .addField("This is a field title, it can hold 256 characters",
+    "This is a field value, it can hold 2048 characters.")
+  /*
+   * Inline fields may not display as inline if the thumbnail and/or image is too big.
+   */
+  .addField("Inline Field", "They can also be inline.", true)
+  /*
+   * Blank field, useful to create some space.
+   */
+  .addBlankField(true)
+  .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
+
 
 var fs = require('fs');
 
@@ -29,7 +57,8 @@ const categ = ["ladder", "club", "roadshow","ranking","krunk-dorm", "others"];
 		case 'ladder':    
          		var com = fs.readFileSync("./source/ladder.txt", {"encoding": "utf-8"});
 	   		message.channel.send(com);
-		    break;
+		    	message.channel.send({embed});
+					     break;
 		case 'roadshow':    
          		var com = fs.readFileSync("./source/roadshow.txt", {"encoding": "utf-8"});
 	   		message.channel.send(com);
