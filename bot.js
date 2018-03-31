ca6b2e4afdab7361eff4c81e499c9324b5193c52
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-03-31 11:03:41 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -158,9 +158,9 @@ const categ = ["ladder", "club", "roadshow","ranking","krunk-dorm", "others"];
  		return message.reply("Category should be one of these:" + (categ));
 		
 	 message.channel.send("Thanks! Your tip will be added after the mods read it. ^^");
-    	var member=398297283831136256;
-        message.member.send("Category:  "+ (cat) ); // send that user a DM
-	message.member.send(" Tip: " + (tip) +" by " + (  message.author.username  ));
+    	var mem=398297283831136256;
+        message.mem.send("Category:  "+ (cat) ); // send that user a DM
+	message.mem.send(" Tip: " + (tip) +" by " + (  message.author.username  ));
 		
 	
 	}
