db9ca1bd2491bd16232379c8a16d167c5be9f71e
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-01 15:44:26 +0800

    Update krunkdorm.js

--- source/krunkdorm.js
+++ source/krunkdorm.js
@@ -1,11 +1,9 @@
-// krunkdorm.js
-
 const Discord = require("discord.js");
 
 var embed = new Discord.RichEmbed()
-  .setTitle("Krunk Dorm")
+  .setTitle("Roadshow")
   .setColor(3447003)
-  .setDescription("FAQ's about the Krunk Dorm")
+  .setDescription("FAQ's about the Roadshow")
   .setFooter("Brought to you by: BlinKcle, weiwei, Steve","https://i.imgur.com/TgNMUlS.jpg")
   .setThumbnail("https://i.imgur.com/TgNMUlS.jpg")
   .setTimestamp()
