62200493d8c919ee82b8c392e54f92fcb218461c
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-01 15:25:35 +0800

    Create achievements.js

---
+++ source/achievements.js
@@ -0,0 +1,26 @@
+
+const Discord = require("discord.js");
+
+var embed = new Discord.RichEmbed()
+  .setTitle("Achievements")
+  .setColor(3447003)
+  .setDescription("FAQ's about Achievements")
+  .setFooter("Brought to you by: BlinKcle, weiwei, Steve","https://i.imgur.com/TgNMUlS.jpg")
+  .setThumbnail("https://i.imgur.com/TgNMUlS.jpg")
+  .setTimestamp()
+  
+  //Add your questions here: Make sure there is a ; only at the end of the last .addField
+  
+  
+  .addField("Q: How often are the songs updated?",
+    "A: Once every 48 hours.  It is worth mentioning that the songs will contain Platinum-only songs, so non-Platinum members get to experience them too.", true);
+/*
+    .addField("Q: Can we pick the song(s) we want to play?", "A: No, the songs are pre-determined and one random song will be chosen for you from the song list.", true);
+  /*
+   * Blank field, useful to create some space..addBlankField(true)
+   */
+
+
+module.exports = Object.freeze({
+  embed: {embed}
+});
