04bec1c55fecb7776537e008417dc27fbee38439
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-01 15:43:20 +0800

    Update achievements.js

--- source/achievements.js
+++ source/achievements.js
@@ -2,9 +2,9 @@
 const Discord = require("discord.js");
 
 var embed = new Discord.RichEmbed()
-  .setTitle("Achievements")
+  .setTitle("Roadshow")
   .setColor(3447003)
-  .setDescription("FAQ's about Achievements")
+  .setDescription("FAQ's about the Roadshow")
   .setFooter("Brought to you by: BlinKcle, weiwei, Steve","https://i.imgur.com/TgNMUlS.jpg")
   .setThumbnail("https://i.imgur.com/TgNMUlS.jpg")
   .setTimestamp()
