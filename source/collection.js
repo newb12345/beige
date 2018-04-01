a7279278768763e9b0e7221b3cc5b484b50b2b90
BLinKcle <37892884+BLinKcle@users.noreply.github.com>
2018-04-01 15:43:52 +0800

    Update collection.js

--- source/collection.js
+++ source/collection.js
@@ -1,10 +1,9 @@
-
 const Discord = require("discord.js");
 
 var embed = new Discord.RichEmbed()
-  .setTitle("Collection")
+  .setTitle("Roadshow")
   .setColor(3447003)
-  .setDescription("FAQ's about the Collection")
+  .setDescription("FAQ's about the Roadshow")
   .setFooter("Brought to you by: BlinKcle, weiwei, Steve","https://i.imgur.com/TgNMUlS.jpg")
   .setThumbnail("https://i.imgur.com/TgNMUlS.jpg")
   .setTimestamp()
